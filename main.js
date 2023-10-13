94e0f29c0f490665546433d1fca93dda52524e30
iskandarusmanov <usman29104@gmail.com>
2023-10-13 18:27:48 +0500

    done

---
+++ main.js
@@ -0,0 +1,111 @@
+//for__1
+
+// let k = 10
+// let n = 6
+// let i = 1
+// while (i <= n) {
+//     console.log(k);
+//     i++
+// }
+
+
+
+//for__2
+
+// let a = 12
+// let b = 19
+// while (a < b) {
+//     a++
+//     console.log(a);
+// }
+
+
+
+//for__4
+
+// let cost = 18000
+// let i = 1
+// while (i <= 10) {
+//     console.log(+(i * cost) + " UZS");
+//     i++
+// }
+
+
+
+//for__6
+
+// let cost = 20000
+// let i = 1
+// while (i <= 2) {
+//     i = i + 0.2
+//     // console.log(i * cost);
+//     console.log(i);
+// }
+
+
+
+//for__7 
+
+// let a = 1
+// let b = 9
+// while (a <= b) {
+//     let m = a
+//     a++
+//     s = m + a
+// }
+// console.log(s);
+
+
+
+//while__1
+
+// let a = 7
+// let b = 26
+// let x = a
+// while (a < b) {
+//     s = b - a
+//      a = a + x
+// }
+// console.log(s);
+
+
+
+//while__2
+
+// let a = 7
+// let b = 26
+// let x = a
+// let y = 0
+// while (a < b) {
+//     y++
+//      a = a + x
+// }
+// console.log(y + " ta joylashtirish mumkin.");
+
+
+
+//while_3
+
+// let a = 9
+// let b = 50
+// let x = a
+// while (a < b) {
+//     s = b - a
+//      a = a + x
+// }
+// console.log("Qoldiq ==> " + s);
+
+
+
+//while_4
+
+let n = 82
+let a = 3
+while (n > 3) { 
+    n = n / a
+}
+if (n == 3) {
+    console.log("This number is dagree of tree");
+ } else {
+        console.log("This number is not dagree of tree"); 
+    }
